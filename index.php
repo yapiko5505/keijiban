1c5b40d2d2753963c30c3bfa41dc6c41c19d6cb4
yapiko <bloomingjewels7021@gmail.com>
2022-04-04 13:00:46 +0900

    keijiban1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>ひと言掲示板</title>
+<link href = "style.css" rel = "stylesheet">
+</head>
+<body>
+<h1>ひと言掲示板</h1>
+<!-- ここにメッセージの入力フォームを設置 -->
+<hr>
+<section>
+<!-- ここに投稿されたメッセージを表示 -->
+</section>
+</body>
+</html>
